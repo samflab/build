15a5b830f1ac404db3885b06130d724c2be81c9d
Masudha Meher <kasturimeh@gmail.com>
2021-08-21 08:48:49 +0530

    added solution to 4th interview ques

--- neog interview sample questions/solutions/4/app.js
+++ neog interview sample questions/solutions/4/app.js
@@ -2,4 +2,17 @@ const userInput = document.getElementById("user-input");
 const h1Btn = document.getElementById("h1-heading");
 const h2Btn = document.getElementById("h2-heading");
 const h3Btn = document.getElementById("h3-heading");
+const outputDiv = document.getElementById("show-output");
+
+h1Btn.addEventListener("click", function(){
+    outputDiv.innerHTML = `<h1>${userInput.value}</h1>`;
+})
+
+h2Btn.addEventListener("click", function(){
+    outputDiv.innerHTML = `<h2>${userInput.value}</h2>`;
+})
+
+h3Btn.addEventListener("click", function(){
+    outputDiv.innerHTML = `<h3>${userInput.value}</h3>`;
+})
 
